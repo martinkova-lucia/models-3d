f870da730bff7e54fd21d933c7dfaa6213c21b7c
martinkova-lucia <lucia.martinkova@student.tuke.sk>
2025-05-30 02:28:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// index.js
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);  // <-- Add this line
   res.end('PayloadCMS EC2 Test Server is Running 🎉');
 });
 
